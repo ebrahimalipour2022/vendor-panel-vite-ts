0c4791a25b94301b0422eff3f3780950eb750118
Alipour.Ebrahim <Alipour.Ebrahim@hasti.com>
2024-08-04 10:47:09 +0330

    remove maps

--- src/pages/dashboard/orders/index.tsx
+++ src/pages/dashboard/orders/index.tsx
@@ -1,20 +1,20 @@
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import Box from '@mui/material/Box';
-import MapComponent from '@/components/neshan-map/neshan-map';
-import MapLocation from '@/assets/icons/MapLocation';
 // sections
-
 // ----------------------------------------------------------------------
 
 export default function Page() {
   const { t } = useTranslation();
+
   return (
     <>
       <Helmet>
         <title> {t('pagesTitle.orders')}</title>
       </Helmet>
-      <MapComponent />
+      {/*<MapComponent />*/}
+      <div>
+        <div className={'w-full h-[70vh]'} />
+      </div>
     </>
   );
 }
