075501f6bafda7f598e7c7c4c270299d96ab4b48
ebrahim <alipour.mc@gmail.com>
2024-08-06 14:26:48 -0800

    add VITE_NESHAN_MAP_API_KEY to env

--- src/components/LeafletMap/index.tsx
+++ src/components/LeafletMap/index.tsx
@@ -1,26 +1,13 @@
-// MapComponent.js
-import { useEffect, useState } from 'react';
+import { LegacyRef, useEffect, useRef, useState } from 'react';
+import L, { LatLngExpression, Map } from 'leaflet';
 import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L, { LatLngExpression } from 'leaflet';
-import classnames from 'classnames';
 import { ILocation, IrMapCoordinates, MapReverseAddressRes } from '@/types';
+import classnames from 'classnames';
 import MapLocation from '@/assets/icons/MapLocation';
-import './style.css';
-import ZapLogo from '@/assets/icons/Logo';
 import axios from 'axios';
-//==========================================================
-// میدان آزادی
-// latitude=35.69978885094379&longitude=51.33797040739728
-//==========================================================
-
-// میدان ونک
-//latitude=35.75753482568149&longitude=51.40995708465471
-//==========================================================
-
-// default location => آزادی
-// useLocation mobile gps
-// web default location
+import ZapLogo from '@/assets/icons/Logo';
+import './style.css';
 
 type Props = {
   position?: ILocation | null;
@@ -29,22 +16,13 @@ type Props = {
   zoom?: number;
 };
 
-// toast.error('آدرس یافت نشد ، لطفا مجدد تلاش نمائید');
-// toast.error('مختصات یافت نشد، لطفا مجدد تلاش نمائید');
-// 51.36281969540723, 35.69672648316882
-
-// GET:
-//     https://api.neshan.org/v5/reverse?lat=LATITUDE&lng=LONGITUDE
-// Headers:
-//  Api-Key: YOUR_API_KEY
-
-//"Api-Key": "service.2Tcr0E5K6MnKAosDGYbmTb5aMiABrcviqDrXcuwU",
+const VITE_NESHAN_MAP_API_KEY = import.meta.env.VITE_NESHAN_MAP_API_KEY;
 
 const getAddressByLatLng = async ({ lat, lng }: { lat: number; lng: number }) => {
   return axios({
     url: `https://api.neshan.org/v5/reverse?lat=${lat}&lng=${lng}`,
     headers: {
-      'Api-Key': 'service.2Tcr0E5K6MnKAosDGYbmTb5aMiABrcviqDrXcuwU',
+      'Api-Key': VITE_NESHAN_MAP_API_KEY,
       Authorization: null,
     },
   });
@@ -56,19 +34,10 @@ const LeafletMapComponent = ({
   onlyView = false,
   zoom = 16,
 }: Props) => {
-  //latitude=35.75753482568149&longitude=51.40995708465471
+  const mapRef = useRef<LegacyRef<Map> | undefined>();
+  const [center, setCenter] = useState<LatLngExpression>(DEFAULT_POSITION); // Default center position
 
-  const [center, setCenter] = useState<LatLngExpression>(
-    ([position?.latitude, position?.longitude] as LatLngExpression) || DEFAULT_POSITION
-  ); // Default center position
-
-  // useEffect(() => {
-  //   if (position) {
-  //     setCenter([position.latitude, position.longitude]);
-  //   }
-  // }, [position]);
-
-  const handleDragEnd = async ({ lat, lng }: IrMapCoordinates) => {
+  async function handleDragEnd({ lat, lng }: IrMapCoordinates) {
     if (lat && lng) {
       setCenter([lat, lng]);
       await getAddressByLatLng({ lat, lng })
@@ -84,15 +53,7 @@ const LeafletMapComponent = ({
         })
         .catch((err) => {});
     }
-  };
-
-  // latitude=35.69978885094379&longitude=51.33797040739728
-  //latitude=35.75753482568149&longitude=51.40995708465471
-
-  // Define the coordinates for the two points
-  const point1: LatLngExpression = [51.40995708465471, 35.75753482568149]; // Example coordinates (latitude, longitude)
-  const point2: LatLngExpression = [51.41423155947801, 35.762504043673815]; // Another set of coordinates
-  const positions: LatLngExpression[] = [point1, point2];
+  }
 
   function GetMapCenterOnDrag() {
     const map = useMapEvents({
@@ -103,13 +64,31 @@ const LeafletMapComponent = ({
     });
     return null;
   }
+
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    // Check if we're on the first render and the position is defined
+    if (isFirstRender && position && mapRef.current) {
+      console.log('mapRef.current', mapRef.current);
+      //@ts-ignore
+      mapRef.current.setView([position.latitude, position.longitude], 16);
+      setIsFirstRender(false); // Set to false so it doesn't run again
+    }
+  }, [isFirstRender, position]);
   return (
     <div
       className={classnames(' h-full w-full relative', {
         'user-select-none pointer-events-none overflow-hidden': onlyView,
       })}
     >
-      <MapContainer center={center} zoom={zoom} style={{ height: '100%', width: '100%' }}>
+      <MapContainer
+        // @ts-ignore
+        ref={mapRef}
+        center={center}
+        zoom={zoom}
+        style={{ height: '100%', width: '100%' }}
+      >
         <GetMapCenterOnDrag />
         {/*<MarkersRandom />*/}
         <TileLayer
@@ -135,3 +114,30 @@ export const customMarkerUserPos = new L.Icon({
   iconAnchor: [5, 20],
   popupAnchor: [2, -40],
 });
+
+//=========================================================
+
+//latitude=35.69978885094379&longitude=51.33797040739728
+//latitude=35.75753482568149&longitude=51.40995708465471
+
+// Define the coordinates for the two points
+// const point1: LatLngExpression = [51.40995708465471, 35.75753482568149]; // Example coordinates (latitude, longitude)
+// const point2: LatLngExpression = [51.41423155947801, 35.762504043673815]; // Another set of coordinates
+// const positions: LatLngExpression[] = [point1, point2];
+
+//==========================================================
+// میدان آزادی
+// latitude=35.69978885094379&longitude=51.33797040739728
+//==========================================================
+
+// میدان ونک
+//latitude=35.75753482568149&longitude=51.40995708465471
+//==========================================================
+
+// default location => آزادی
+// useLocation mobile gps
+// web default location
+
+// toast.error('آدرس یافت نشد ، لطفا مجدد تلاش نمائید');
+// toast.error('مختصات یافت نشد، لطفا مجدد تلاش نمائید');
+// 51.36281969540723, 35.69672648316882
