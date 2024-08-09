99fa79a7e2992c0b35dd610ca50686ab21a0a769
ebrahim <alipour.mc@gmail.com>
2024-08-08 21:25:45 -0800

    add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import checker from 'vite-plugin-checker';
 import { loadEnv, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const PORT = 3030;
 
@@ -47,6 +48,39 @@ export default defineConfig(({ mode }: { mode: string }) => {
       }),
       svgr({
         include: '**/*.svg?react',
+      }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        manifest: {
+          name: 'پنل تامین کنندگان زپ',
+          short_name: 'zap vendor panel',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
       }),
     ],
     resolve: {
